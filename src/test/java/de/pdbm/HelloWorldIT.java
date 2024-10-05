0538575296698f31020c2cb280b77bee1ba82e79
Luca Arnecke <luca@arnecke.name>
2024-10-05 20:20:50 +0200

    IT delete

--- src/test/java/de/pdbm/HelloWorldIT.java
+++ src/test/java/de/pdbm/HelloWorldIT.java
@@ -6,14 +6,27 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldIT {
+    Client client;
+
+    @BeforeEach
+    public void before() {
+        client = ClientBuilder.newClient();
+    }
+
+    @AfterEach
+    public void after() {
+        client.close();
+    }
+
     @Test
     public void getHelloWorld() {
-        Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080/starter/api/helloworld");
         Response response = target.request(MediaType.APPLICATION_JSON).get(Response.class);
 
@@ -21,5 +34,13 @@ public class HelloWorldIT {
 
         JsonObject requestBody = response.readEntity(JsonObject.class);
         assertEquals("Hello World", requestBody.getString("message"));
+    }
+
+    @Test
+    public void delete() {
+        WebTarget target = client.target("http://localhost:8080/starter/api/helloworld/1");
+        try (Response response = target.request(MediaType.APPLICATION_JSON).delete(Response.class)) {
+            assertEquals(204, response.getStatus());
+        }
     }
 }
